1a76d70e4888911c200a025ea184e366298431ef
diegomenezesfernandes@outlook.com <diegomenezesfernandes@outlook.com>
2021-08-30 14:56:19 -0300

    Adicionando o swagger na aplicacao

--- DesignPatterns.Web/Startup.cs
+++ DesignPatterns.Web/Startup.cs
@@ -26,13 +26,24 @@ namespace DesignPatterns.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             Injection.RegisterService(services);
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
---
+++ README.txt
@@ -0,0 +1,3 @@
+
+Swagger:
+https://docs.microsoft.com/pt-br/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-5.0&tabs=visual-studio
